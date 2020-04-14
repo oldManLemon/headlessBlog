4969cd5ff21eb3f71056bfa55175f5b7fe3ecad4
Sam Macpherson <sam.mack91@gmail.com>
2020-04-14 20:11:15 +1000

    grab user via api

--- frontend/src/components/PostPage.js
+++ frontend/src/components/PostPage.js
@@ -4,31 +4,41 @@ import axios from 'axios'
 export class PostPage extends Component {
     state = {
         post: {},
-        author:'',
+        author:{},
+        comments: {},
         isLoaded: false
 
 
     }
     componentDidMount() {
+
+        axios.get(`/wp-json/wp/v2/comments?post=${this.props.match.params.id}`)
+            .then(x => {
+                this.setState({ comments: x.data });
+            });
         
-        // const getAuthor = axios.get(`/wp-json/wp/v2/users/${this.props.match.params.author}`);
         axios.get(`/wp-json/wp/v2/posts/${this.props.match.params.id}`)
-            .then(res => this.setState(
-                {
-                    post: res.data,
-                    isLoaded: true
-                }
-            )
-            )
+            .then(res => {
+
+                this.setState( { post: res.data, author: null, isLoaded: false });
+
+                axios.get(`/wp-json/wp/v2/users/${res.data.author}`)
+                .then(x => {
+                    this.setState(
+                                    {
+                                        post: res.data,
+                                        author: x.data,
+                                        isLoaded: true
+                                    }
+                                );
+                });
+
+            }  )
             .catch(err => console.log(err));
-        
-        
-
-
 
     }
     render() {
-        const { post, isLoaded } = this.state
+        const { post, author, isLoaded } = this.state
         if (isLoaded) {
             console.log(post)
             return (
@@ -36,13 +46,10 @@ export class PostPage extends Component {
                     <Link to='/'> Go Back</Link>
                     <h1>{post.title.rendered}</h1>
                     {/* Crap fix this */}
-                    <small>by <strong>{post.author}</strong></small>
+                    <small>by <strong>{author.name}</strong></small>
                     <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-
+                    <div>{comments.}</div>
                 </div>
-
-
-
             )
         }
 
